578581610479ffcb19ba8af72ba32e66876c111c
Ashton <as12984@ms.wasatch.edu>
2022-11-30 10:04:52 -0700

    Got the ship a sail

---
+++ functions.js
@@ -0,0 +1,23 @@
+
+
+function putGolf(){
+
+    window.alert("Do you like golf?");
+
+}
+
+function stylisticChange(){
+
+    var textBox = document.getElementById("text");
+    textBox.style.backgroundColor = "green";
+
+}
+
+function Soulcapture(){
+    window.alert("Per your agreement we have sent your soul to amazon (shipping fees stil apply)")
+}
+
+function appear(){
+    var color = document.getElementById("Next");
+    color.style.backgroundColor = "lightblue";
+}
